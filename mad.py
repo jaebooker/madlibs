f71eb2151688cc4460b7d2ed247387e949566ccd
jaebooker <jaesonbooker@gmail.com>
2018-08-29 17:04:05 -0700

    Madlibs

---
+++ mad.py
@@ -0,0 +1,12 @@
+word1 = raw_input("Enter a name: ")
+word2 = raw_input("Enter a verb: ")
+word3 = raw_input("Enter another verb: ")
+word4 = raw_input("Enter a noun: ")
+word5 = raw_input("Enter an ajective: ")
+word6 = raw_input("Enter a verb: ")
+word7 = raw_input("Enter a noun: ")
+word8 = raw_input("Enter a verb: ")
+word9 = raw_input("Enter a verb: ")
+word10 = raw_input("Enter an ajective: ")
+story = word1 + " walked into MakeSchool, hoping to " + word2 + ", or at least " + word3 + ". Carrying only a " + word4 + ". Taking off their shoes, the " + word5 + " expression on the students' faces meant they were certainly going to " + word6 + ". However, the " + word7 + " had no interest to " + word8 + ", instead choosing to " + word9 + " " + word10
+print(story)
