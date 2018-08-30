69d398c4e6dfe8a1e0540c7f9990dc230960df9a
jaebooker <jaesonbooker@gmail.com>
2018-08-29 17:15:25 -0700

    Madlibs test added

--- mad.py
+++ mad.py
@@ -10,7 +10,7 @@ word9 = raw_input("Enter a verb: ")
 word10 = raw_input("Enter an ajective: ")
 story = word1 + " walked into MakeSchool, hoping to " + word2 + ", or at least " + word3 + ". Carrying only a " + word4 + ". Taking off their shoes, the " + word5 + " expression on the students' faces meant they were certainly going to " + word6 + ". However, the " + word7 + " had no interest to " + word8 + ", instead choosing to " + word9 + " " + word10
 print(story)
-func test():
+def test():
     print("Running test")
     word1 = "Jerremy"
     word2 = "run"
@@ -23,4 +23,4 @@ func test():
     word9 = "die"
     word10 = "gracefully"
     print(story)
-test()
+#test()
