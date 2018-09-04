5ee46dac4a8b7682f7d64bc821715f1f819d3817
jaebooker <jaesonbooker@gmail.com>
2018-09-04 13:57:18 -0700

    finishing touches

--- this_is.py
+++ this_is.py
@@ -1,2 +1,2 @@
-word = [raw_input("Enter an adjective "), raw_input("Enter a proper noun ")]
+word = [raw_input("Enter an adjective "), raw_input("Enter a place ")]
 print(word[0] + "?!" + " THIS IS " + word[1].upper() + "!")
